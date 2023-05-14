08138a5b57d4846b81350039848c95682496b4e9
nastyasha_18 <ateshkina@bk.ru>
2023-05-15 00:14:44 +0500

    Комментирование частей кода

--- ant.js
+++ ant.js
@@ -1,98 +1,229 @@
-var cities = [];
-var totalCities = 0;
-var recordDistance;
-var bestEver;
-var drawPath = false;
-
-function setup() {
-  var canvas = createCanvas(400, 300);
-  canvas.parent('canvas-container');
-  canvas.mouseClicked(addCity);
-
-  var findButton = createButton('Find Path');
-  findButton.parent('canvas-container');
-  findButton.mousePressed(findPath);
-  findButton.position(canvas.x + (canvas.width / 2) - (findButton.width / 2), canvas.y + canvas.height + 20);
+const canvas = document.getElementById("Canvas"); // Получаем элемент canvas и контекст для рисования
+const ctx = canvas.getContext("2d");
+const size_window = innerWidth; // Устанавливаем размеры canvas в зависимости от ширины окна
+if(innerWidth < 1000){
+  canvas.height=400;
+  canvas.width=400;
+} else{
+  canvas.height=400;
+  canvas.width=400;
 }
+// Заполняем canvas белым цветом и рисуем прямоугольник с черной обводкой
+ctx.fillStyle = "#fff";  
+ctx.fillRect(0, 0, canvas.width, canvas.height); 
+ctx.strokeStyle = 'black'; // цвет обводки
+ctx.strokeRect(0, 0, canvas.width, canvas.height); // рисуем прямоугольник
 
-function draw() {
-  background(0);
-  fill(255);
+window.addEventListener(`resize`, event => {    // Обработчик события изменения размера окна
 
-  for (var i = 0; i < cities.length; i++) {
-    ellipse(cities[i].x, cities[i].y, 8, 8);
+  if(window.innerWidth < 1000){
+    canvas.height=400;
+    canvas.width=400;
+    ctx.fillStyle = "#fff"; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+  } else {
+    canvas.height=400;
+    canvas.width=400;
+    ctx.fillStyle = "#fff"; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+    ctx.strokeStyle = 'black'; // цвет обводки
+    ctx.strokeRect(0, 0, canvas.width, canvas.height); // рисуем прямоугольник
+    
   }
-
-  if (drawPath) {
-    stroke(255);
-    strokeWeight(1);
-    noFill();
-    beginShape();
-    for (var i = 0; i < cities.length; i++) {
-      vertex(cities[i].x, cities[i].y);
+    }, false);
+    
+     canvas.addEventListener("click", function(event) { // Обработчик события клика по canvas
+   
+    let x = event.offsetX;
+    let y = event.offsetY;
+    
+    
+    ctx.fillStyle = "black";  // Рисуем точку на canvas и добавляем ее координаты в массив Points
+    ctx.beginPath(); 
+    ctx.arc(x, y, 4, 0, Math.PI * 2, true); 
+    ctx.fill(); 
+    Points.push({x: x, y: y});
+  });
+    let massiv2 = [];      // Инициализация переменных
+    let Points = [];
+    let Distance = [];
+    let visitedNodes = [];
+    let pheromone = [];
+    let CityArray = [];
+    let massiv = [];
+    let TotalDist = [];
+    let last = 100000;
+    let res = 0;
+    let alpha =2;
+    let beta = 3;
+    let coefficient = 0.2;
+    let Q = 30;
+  
+    function DrawLine(population, Points) {   // Функция для рисования линий между точками
+    
+    for(let i = 0; i < Points.length-1; i++) {
+    ctx.beginPath(); 
+    ctx.moveTo(Points[population[i]].x,Points[population[i]].y); 
+    ctx.lineTo(Points[population[i+1]].x,Points[population[i+1]].y); 
+    ctx.stroke(); 
     }
-    endShape(CLOSE);
-    stroke(255, 0, 255);
-    strokeWeight(4);
-    noFill();
-    beginShape();
-    for (var i = 0; i < cities.length; i++) {
-      vertex(bestEver[i].x, bestEver[i].y);
-    }
-    endShape(CLOSE);
-  }
+    ctx.beginPath();
+    ctx.moveTo(Points[population[population.length-1]].x,Points[population[population.length-1]].y ); 
+    ctx.lineTo(Points[population[0]].x,Points[population[0]].y); 
+    ctx.stroke(); 
+  };
 
-  if (totalCities > 1) {
-    var i = floor(random(cities.length));
-    var j = floor(random(cities.length));
-    swap(cities, i, j);
-    var d = calcDistance(cities);
-    if (d < recordDistance) {
-      recordDistance = d;
-      bestEver = cities.slice();
+  function CalculateDist(TotalDist, CA, Points) {    // Функция для вычисления расстояния между точками 
+    let sum = 0;
+    
+    for(let i = 0;i < Points.length-1 ; i++) {
+      sum += Math.sqrt(Math.pow(Points[CA[i+1]].x - Points[CA[i]].x,2) + Math.pow(Points[CA[i+1]].y - Points[CA[i]].y,2));
     }
-  }
-}
+    sum += Math.sqrt(Math.pow(Points[CA[0]].x - Points[CA[Points.length-1]].x,2) + Math.pow(Points[CA[0]].y - Points[CA[Points.length-1]].y,2));
+  
+    return sum;
+  };
 
-function addCity() {
-  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    var v = createVector(mouseX, mouseY);
-    cities.push(v);
-    totalCities++;
-    if (totalCities == 1) {
-      recordDistance = 0;
-      bestEver = cities.slice();
-    }
-
-    if (totalCities > 1) {
-      var d = calcDistance(cities);
-      if (d < recordDistance) {
-        recordDistance = d;
-        bestEver = cities.slice();
+  function RefreshPheromone(pheromone, Q, CA , TotalDist, coefficient) {  // Функция для обновления уровня феромонов
+    for(let i = 0; i < Points.length;i++) {
+      for(let j = 0; j< Points.length;j++) {
+        if(i !== j) 
+          pheromone[i][j]*= coefficient;
       }
     }
-  }
-}
 
-function findPath() {
-  drawPath = true;
-}
-
-function swap(a, i, j) {
-  var temp = a[i];
-  a[i] = a[j];
-  a[j] = temp;
-}
-
-function calcDistance(points) {
-  var sum = 0;
-  for (var i = 0; i < points.length - 1; i++) {
-    var d = dist(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
-    sum += d;
+    let massiv1;
+    let pheromonechange;
+    for(let i = 0; i < TotalDist.length;i++) {   // Увеличиваем уровень феромонов на ребрах, пройденных муравьями
+      pheromonechange = Q/ TotalDist[i];
+      massiv1 = [...CityArray[i]];
+    for(let j = 0; j < Points.length-1;j++) {
+      {
+      pheromone[massiv1[j]][massiv1[j+1]] += pheromonechange;
+      pheromone[massiv1[j+1]][massiv1[j]] += pheromonechange;
+      }
+    } 
+    pheromone[massiv1[Points.length-1]][massiv1[0]]+= pheromonechange;  
+    pheromone[massiv1[0]][massiv1[Points.length-1]]+= pheromonechange;   
   }
 
-  // Добавляем расстояние между последней и первой точками
-  sum += dist(points[points.length - 1].x, points[points.length - 1].y, points[0].x, points[0].y);
+    return pheromone;
+  };
 
-  return sum;
-}
+  function selectNextNode(currentNode, pheromone, distance, alpha, beta, visitedNodes) {   // Функция для выбора следующей точки муравья
+  let probabilities = [];
+  let sum = 0;
+  for (let i = 0; i < pheromone.length; i++) { // Вычисляем вероятность перехода в каждую непосещенную точку
+    if (i !== currentNode && !visitedNodes.includes(i)) {
+      let attractiveness = Math.pow((1 / distance[currentNode][i]), beta); // Привлекательность точки
+      let pheromoneLevel = Math.pow(pheromone[currentNode][i], alpha);// Уровень феромонов на ребре
+      let probability = pheromoneLevel * attractiveness; // Вычисляем вероятность перехода в точку
+      probabilities.push({ node: i, probability: probability }); // Добавляем вероятность в массив
+      sum += probability; // Добавляем вероятность к сумме
+    }
+  }
+  probabilities.forEach((p) => (p.probability /= sum)); // Нормализуем вероятности
+  probabilities.sort((a, b) => b.probability - a.probability); // Сортируем вероятности по убыванию
+  let rand = Math.random(); // Выбираем следующую точку на основе вероятностей
+  let cumulativeProbability = 0;
+  for (let i = 0; i < probabilities.length; i++) {
+    cumulativeProbability += probabilities[i].probability;
+    if (rand <= cumulativeProbability) {
+      return probabilities[i].node;
+    }
+  }
+};
+
+        let iteration = 0; //инициализируем переменную iteration для отслеживания количества итераций
+    
+        function AntAlgorithm () {  // Функция для запуска алгоритма муравьиной колонии
+  
+          let Ants = 4000; // Задаем количество муравьев
+          
+          for (let i = 0; i < Points.length; i++) { // Инициализируем матрицу расстояний между точками
+            Distance[i] = [];
+        for (let j = 0; j < Points.length; j++) { 
+          Distance[i][j] = 0;
+        }
+      }
+      for(let i = 0;i < Points.length ;i++) 
+        for(let j = 0; j < Points.length;j++) // Вычисляем расстояние между каждой парой точек
+          if(i !== j) 
+            Distance[i][j] = Math.sqrt(Math.pow(Points[j].x - Points[i].x,2) + Math.pow(Points[j].y - Points[i].y,2));
+          
+            for (let i = 0; i < Points.length; i++) { // Инициализируем уровень феромонов на всех ребрах
+            pheromone[i] = [];
+        for (let j = 0; j < Points.length; j++) {
+          pheromone[i][j] = 0;
+        }
+      }
+      for(let i = 0;i < Points.length ;i++) 
+        for(let j = 0; j < Points.length;j++) 
+          if(i !== j) 
+            pheromone[i][j] = 0.2;
+            
+      let count = 0;
+      res = 0;
+      iteration = 0;
+      while(iteration < 10000 && res < 14) {
+        console.log("итераций");
+        CityArray = [];
+        iteration++;
+        let currentNode = 0;
+        
+        for(let i = 0; i < Ants;i++ ) {  // Запускаем цикл для запуска муравьев на поиск оптимального маршрута
+          if (count == Points.length)
+            count = 0;
+          visitedNodes = [];
+          massiv = [];
+          currentNode = count;
+        for(let j = 0 ;j < Points.length-1 ;j++)  // Запускаем муравья на выбор следующей точки маршрута
+        { massiv.push(currentNode);
+          visitedNodes.push(currentNode);
+          currentNode = selectNextNode(currentNode, pheromone, Distance, alpha, beta, visitedNodes);  
+          
+          
+        }
+        massiv.push(currentNode);
+        CityArray.push(massiv);
+        count++;
+      }
+      for(let i = 0 ;i < CityArray.length; i++) {    // Вычисляем расстояние для каждого маршрута и выбираем оптимальный
+      TotalDist[i] = CalculateDist(TotalDist, CityArray[i], Points);  
+      }
+      
+      pheromone = RefreshPheromone(pheromone, Q, CityArray , TotalDist, coefficient); // Обновляем уровень феромонов на ребрах
+
+      for(let i = 0;i < TotalDist.length-1;i++) {
+            for(let j = 0 ; j < TotalDist.length - i - 1;j++) {
+              if (TotalDist[j]> TotalDist[j+1]) {
+                let temp = TotalDist[j];
+                TotalDist[j] = TotalDist[j+1];
+                TotalDist[j+1] = temp;
+                let tmp = CityArray[j];
+                CityArray[j] = CityArray[j+1];
+                CityArray[j+1] = tmp;
+              }
+            }
+          }
+      
+          
+          if(last <= TotalDist[0])   // Проверяем, является ли текущий маршрут лучшим, чем предыдущий
+          {
+              res++;
+          }
+          else  
+          {
+              res = 0;
+              last = TotalDist[0];
+              for(let i = 0 ;i < Points.length ;i++)
+              massiv2[i] = CityArray[0][i];
+          }
+          
+          
+      
+  }
+ 
+      DrawLine(massiv2, Points); // Рисуем оптимальный маршрут на canvas
+
+};
--- astarb.js
+++ astarb.js
@@ -1,6 +1,7 @@
 // A* algorithm (CODINGAN SENDIRI)
 
-function astar(board, start, destination) {
+function astar(board, start, destination) // Функция A* для поиска кратчайшего пути на доске board от точки start до точки destination
+{
     console.log('A* board', board);
     function reconstructPath(cameFrom, current, board) {
        let path = [current];
@@ -19,37 +20,48 @@ function astar(board, start, destination) {
        const y1 = n[1];
        const y2 = destination[1];
        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+       /* функция вычисляет эвристическое значение (оценку стоимости пути от текущей вершины "n" 
+       до пункта назначения). В качестве входных параметров она принимает координаты текущей вершины (x, y)
+        и координаты пункта назначения (destination). Затем она возвращает сумму абсолютных разностей между 
+        x-координатами и y-координатами, которая представляет Манхэттенское расстояние между ними*/
     }
  
-    function nodeToKey(n) {
+    function nodeToKey(n) 
+    {
        return `${n[0]},${n[1]}`;
+       /* функция преобразует координаты вершины (x, y) в строковый ключ в формате "x,y", который будет 
+       использоваться для доступа к вершине в hashmap, используемой алгоритмом для отслеживания посещенных вершин.*/
     }
  
-    // очередб приоритетов
-    let openSet = [start];
-    let cameFrom = new Map();
+    let openSet = [start]; //это список, содержащий только начальную вершину start
+    let cameFrom = new Map(); /* пустой словарь Map(), который будет использоваться для
+     отслеживания того, какая вершина вела к какой вершине во время алгоритма поиска пути*/
  
-    // O(n^2)
+   //словарь - который будет хранить текущие значения стоимости пути от стартовой точки до каждой точки на доске
     let gScore = {};
     for (let y = 0; y < board.length; y++) {
        for (let x = 0; x < board[y].length; x++) {
-          gScore[`${x},${y}`] = Infinity;
+          gScore[`${x},${y}`] = Infinity; // Изначально стоимость пути до каждой точки на доске равна бесконечности
        }
     }
-    // для узла n, fScore[n] := gScore[n] + h(n). fScore[n] представляет собой наше нынешнее лучшее относительное положение 
-    // Насколько маленьким может быть путь от начала до конца, если он проходит через N.
+
+    /* словарь - который будет хранить текущие значения оценки стоимости пути от стартовой точки
+     до конечной точки через каждую точку на доске*/
     let fScore = Object.assign({}, gScore);
  
-    gScore[nodeToKey(start)] = 0;
-    fScore[nodeToKey(start)] = h(start);
+    gScore[nodeToKey(start)] = 0; // Устанавливаем стоимость пути от стартовой точки до самой себя равной 0
+    fScore[nodeToKey(start)] = h(start); // Устанавливаем оценку стоимости пути от стартовой точки до конечной точки через стартовую точку
  
-    while (openSet.length > 0) {
-       const current = openSet.pop();
-       if (current[0] == destination[0] && current[1] == destination[1]) {
+    while (openSet.length > 0) // Итерируемся по openSet, пока он не станет пустым
+    {
+       const current = openSet.pop(); // Извлекаем из openSet точку с наименьшей оценкой
+      // Если текущая точка является конечной точкой, то возвращаем найденный путь
+       if (current[0] == destination[0] && current[1] == destination[1]) 
+      {
           return reconstructPath(cameFrom, current, board);
        }
  
-       let neighbors = [];
+       let neighbors = []; // Получаем соседей текущей точки
        if (board[current[1]] !== undefined) {
           const left = board[current[1]][current[0] - 1];
           if (left !== undefined && left !== 0) {
@@ -74,28 +86,33 @@ function astar(board, start, destination) {
              neighbors.push([current[0], current[1] + 1]);
           }
        }
+
+       // Обновляем значения стоимости пути и оценки стоимости пути для каждого соседа текущей точки
        for (let neighbor of neighbors) {
  
-          // tentative_gScore - расстояние от старта до соседа через ток
-          const tentative_gScore = gScore[nodeToKey(current)] + 1; // gScore[current] + d(current, neighbor), d(current,neighbor) is the weight of the edge from current to neighbor
- 
+          const tentative_gScore = gScore[nodeToKey(current)] + 1; 
+         
+          // Если стоимость пути до соседа еще не была вычислена, то устанавливаем ее равной бесконечности
           gScore[nodeToKey(neighbor)] = gScore[nodeToKey(neighbor)] === undefined ? Infinity : gScore[nodeToKey(neighbor)];
- 
-          if (tentative_gScore < gScore[nodeToKey(neighbor)]) {
-             // Этот путь к соседу лучше любого предыдущего.
+         
+          // Если новая стоимость пути до соседа меньше текущей, то обновляем значения стоимости пути и оценки стоимости пути для соседа
+
+          if (tentative_gScore < gScore[nodeToKey(neighbor)])
+           {
              cameFrom.set(nodeToKey(neighbor), current);
              gScore[nodeToKey(neighbor)] = tentative_gScore;
              fScore[nodeToKey(neighbor)] = tentative_gScore + h(neighbor);
- 
-             if (!openSet.find(x => x[0] === neighbor[0] && x[1] === neighbor[1])) {
-                // Добавить в приоритетную очередь 
-                // arr.sort( (a, b) => b - a );
+
+             // Если сосед еще не находится в openSet, то добавляем его туда и сортируем openSet по убыванию оценки fScore
+             if (!openSet.find(x => x[0] === neighbor[0] && x[1] === neighbor[1])) 
+             {
                 openSet.push(neighbor);
                 openSet.sort((a, b) => fScore[nodeToKey(b)] - fScore[nodeToKey(a)]);
              }
           }
        }
     }
+    // Если openSet стал пустым, то возвращаем null в качестве пути и исходную доску board
     console.log('SAMPE SINI!', board);
     return { path: null, board };
  }
@@ -121,21 +138,30 @@ function astar(board, start, destination) {
  
  // ========= html =============
  
+ //объявление переменных и получение элементов HTML-кода страницы:
  const $xInput = document.getElementById('x-length');
  const $yInput = document.getElementById('y-length');
  const $btnGenerate = document.getElementById('btn-generate');
- let x = 1;
- let y = 1;
- let generatedBoard = [];
- let start = { node: null };
- let end = { node: null };
+ let x = 1; //ширина
+ let y = 1;// длина
+ let generatedBoard = []; //генерация
+ let start = { node: null }; //начало
+ let end = { node: null }; //конец
  
- function generateBoard() {
-    generatedBoard = [];
+ //функция генерации доски
+ function generateBoard() 
+ {
+    generatedBoard = []; // Очищаем сгенерированную доску и информацию о стартовой и конечной точках
     start = { node: null };
     end = { node: null };
+
+    // Итерируемся по высоте и ширине доски, генерируя случайные значения для каждой ячейки
     for (let i = 0; i < y; i++) {
        generatedBoard[i] = [];
+
+       /* Если случайное значение больше 2, то устанавливаем значение ячейки равным 1 (проходимой), 
+       иначе - равным 0 (непроходимой)*/
+
        for (let j = 0; j < x; j++) {
           const rand = Math.round(Math.random() * 10);
           generatedBoard[i][j] = rand > 2 ? 1 : 0;
@@ -149,13 +175,15 @@ function astar(board, start, destination) {
  renderBoard(generatedBoard, start, end);
  
  
- function maxCap(e) {
+ function maxCap(e) // Функция для ограничения максимального значения input
+ {
     e.stopPropagation();
     if (e.target.value > 30) {
        e.target.value = 30;
     }
  }
- function minCap(e) {
+ function minCap(e) // Функция для ограничения минимального значения input
+ {
     e.stopPropagation();
     if (e.target.value < 2) {
        e.target.value = 2;
@@ -165,63 +193,74 @@ function astar(board, start, destination) {
  $xInput.addEventListener('input', maxCap);
  $xInput.addEventListener('change', minCap);
  $yInput.addEventListener('input', maxCap);
+
  $yInput.addEventListener('change', minCap);
- $btnGenerate.addEventListener('click', () => {
+ //получает значения ширины и высоты доски из элементов input с id "x-length" и "y-length"
+ $btnGenerate.addEventListener('click', () => 
+ {
     x = Number($xInput.value);
     y = Number($yInput.value);
-    generateBoard();
+    generateBoard(); //генерирует  и отображает новую доску
     renderBoard(generatedBoard, start, end);
     path = null;
  });
- 
- 
- 
+
+
+ // Определим функцию, которая отображает на доске лабиринт и маршрут
+
  function renderBoard(b, start, end) {
     const $board = document.getElementById('board');
     $board.innerHTML = '';
     const yLength = b.length;
     const xLength = b[0].length;
+    // Определим ширину и высоту доски в соответствии с размером лабиринта
     const boardWidth = 20 * xLength;
     const boardHeight = 20 * yLength;
- 
+   // Определим сетку доски (ряды и колонки)
     $board.style.gridTemplateRows = `repeat(${yLength}, 20px)`;
     $board.style.gridTemplateColumns = `repeat(${xLength}, 20px)`;
     $board.style.width = `${boardWidth}px`;
     $board.style.height = `${boardHeight}px`;
- 
-    for (let y = 0; y < b.length; y++) {
-       for (let x = 0; x < b[y].length; x++) {
+
+    for (let y = 0; y < b.length; y++) // Пробежимся по всему лабиринту
+    {
+       for (let x = 0; x < b[y].length; x++) 
+       {
+          // Создаем div для каждого блока (клетки) и добавляем класс
           const $box = document.createElement('div');
           $box.classList.add('box');
- 
+
+          // Определяем, является ли блок стеной или нет
           if (b[y][x] === 0) $box.classList.add('disabled');
  
- 
-          // цвет выводв для узла пути
+          // Определяем маршрут
           if (start.node !== null && end.node !== null) {
              const isStart = x === start.node[0] && y === start.node[1];
              const isEnd = x === end.node[0] && y === end.node[1];
              if (b[y][x] === 2 && !isStart && !isEnd) $box.classList.add('path');
           }
-          // начальный узел вывода
+      
+           // Определяем начальную точку
           if (start.node !== null) {
              const isStart = x === start.node[0] && y === start.node[1];
              if (isStart) { $box.classList.add('start'); }
           }
-          // конечный узел вывода
+    
+          // Определяем конечную точку
           if (end.node !== null) {
              const isEnd = x === end.node[0] && y === end.node[1];
              if (isEnd) { $box.classList.add('end'); }
           }
+
+
+          $box.dataset.x = x; $box.dataset.y = y; // Добавляем координаты блока
  
- 
- 
-          $box.dataset.x = x; $box.dataset.y = y;
- 
-          // event listener для начального/конечного узла настройки
+          // Добавляем обработчик событий на клик по блоку
           $box.addEventListener('click', (e) => {
              const $boxes = document.querySelectorAll('.box');
-             if (isSelectingStart) {
+             if (isSelectingStart)
+            {
+               // Определяем начальную точку
                 if (start.node !== null) {
                    $boxes[start.node[1] * xLength + start.node[0]].classList.remove('start')
                 }
@@ -231,7 +270,9 @@ function astar(board, start, destination) {
                 $box.classList.remove('end', 'disabled');
  
              }
-             if (isSelectingEnd) {
+             if (isSelectingEnd) 
+             {
+               // Определяем конечную точку
                 if (end.node !== null) {
                    $boxes[end.node[1] * xLength + end.node[0]].classList.remove('end')
                 }
@@ -242,7 +283,7 @@ function astar(board, start, destination) {
 
              }
  
-             // рисующая стена/отключенный узел 
+             // Определяем запретную зону
              if (isSelectingWall) {
                 const { x, y } = $box.dataset;
                 if ($box.classList.contains('start')) {
@@ -253,7 +294,7 @@ function astar(board, start, destination) {
                 }
                 $box.classList.remove('start', 'end', 'path');
                 $box.classList.toggle('disabled');
-                // переключение стены/пути в двухмерном массиве board
+            
                 if (b[y][x] === 1) {
                    b[y][x] = 0;
                 } else if (b[y][x] === 0 || b[y][x] === 2) {
@@ -262,17 +303,17 @@ function astar(board, start, destination) {
              }
  
           });
+          // Добавляем созданный блок на доску
           $board.append($box);
        }
     }
  
  }
  
- // пусть начнется = {node: [0, 0]};
- // пусть закончится= {node: [9, 9]};
- let path = null; // the path of start to end after executing start button
  
+ let path = null; // Путь от начала до конца после выполнения кнопки «Пуск»
  
+ // Получение элементов DOM
  const $start = document.getElementById('start');
  const $clear = document.getElementById('clear');
  const $startInfo = document.getElementById('start-info');
@@ -282,7 +323,7 @@ function astar(board, start, destination) {
  let isSelectingEnd = false;
  let isSelectingWall = false;
  
- 
+ // Флаги выбора стартовой, конечной и стенной точек
  $wallInfo.addEventListener('click', (e) => {
     isSelectingWall = !isSelectingWall;
  
@@ -291,6 +332,7 @@ function astar(board, start, destination) {
     $endInfo.querySelector('.wrapper').classList.remove('active');
     $startInfo.querySelector('.wrapper').classList.remove('active');
  
+    // Обработчики событий клика
     $wallInfo.querySelector('.wrapper').classList.toggle('active');
  });
  $startInfo.addEventListener('click', (e) => {
@@ -315,11 +357,11 @@ function astar(board, start, destination) {
     $endInfo.querySelector('.wrapper').classList.toggle('active');
  });
  
- 
- 
+ // Функция очистки найденного пути
  function clearPaths() {
     if (path !== null) {
        let $boxes = document.querySelectorAll('.box');
+       // Удаление класса 'path' у ячеек, входящих в путь
        for (let i = 0; i < path.length; i++) {
  
           const node = path[i];
@@ -334,7 +376,7 @@ function astar(board, start, destination) {
     path = null;
  }
  
- 
+// Обработчик события клика на кнопку 'start'
  $start.addEventListener('click', () => {
     clearPaths();
  
@@ -346,13 +388,13 @@ function astar(board, start, destination) {
        return alert('Добавьте стартовую и конечную точки');
     }
  
-    // после выполнения алгоритма*
+     // Запуск алгоритма поиска пути
     const { board: newBoard, path: p } = astar(generatedBoard, start.node, end.node);
     path = p;
-    // генерация новой доски 
+    // Генерация новой доски и прокладка найденного пути 
     if (path !== null) {
        let $boxes = document.querySelectorAll('.box');
-       // проложить путь 
+         // Визуализация найденного пути с задержкой на каждую ячейку 
        for (let i = 0; i < path.length; i++) {
           setTimeout(() => {
              const node = path[i];
@@ -368,6 +410,7 @@ function astar(board, start, destination) {
     }
  
  });
+ // Обработчик события клика на кнопку 'clear'
  $clear.addEventListener('click', () => {
     clearPaths();
  });
