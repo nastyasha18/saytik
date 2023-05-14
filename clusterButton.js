509e07882f1ee46732aff9433461b7a78375211e
nastyasha_18 <ateshkina@bk.ru>
2023-05-14 17:59:53 +0500

    отредактировала код

--- clusterButton.js
+++ clusterButton.js
@@ -1,113 +1,138 @@
-const canvas = document.getElementById('canvas'); // Получаем элемент холста из DOM
-const ctx = canvas.getContext('2d'); // Получаем контекст для рисования на холсте
-const clusterButton = document.getElementById('clusterButton'); // Получаем кнопку для кластеризации точек
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const clusterButton = document.getElementById('clusterButton');
+const resetButton = document.getElementById('resetButton');
 
-// Размер кластеров
-const clusterSize = 800;
-
-// Массив точек
 let points = [];
+let clusters = [];
 
-// Функция для добавления точки на холсте
-function addPoint(event) 
+// Количество кластеров
+const k = 3;
+
+function addPoint(event)
 {
-  const rect = canvas.getBoundingClientRect(); // Получаем координаты холста
-  const x = event.clientX - rect.left; // Получаем координаты точки
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
 
-  points.push({x, y}); // Добавляем точку в массив
-  drawPoints(); // Перерисовываем точки на холсте
+  points.push({ x, y });
+  drawPoints();
 }
 
-// Отрисовка точек на холсте
-function drawPoints() 
-{
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Очищаем холст перед перерисовкой
-  ctx.fillStyle = '#000';  // Задаем стиль для рисования точек
+function drawPoints() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#000';
 
-  for (let i = 0; i < points.length; i++)  // Отрисовываем все точки в массиве points
-  {  
-    ctx.beginPath(); // Начинаем новый путь
-    ctx.arc(points[i].x, points[i].y, 5, 0, Math.PI * 2); // Рисуем окружность вокруг точки
-    ctx.fill(); // Заливаем окружность цветом
+  for (let i = 0; i < points.length; i++) {
+    ctx.beginPath();
+    ctx.arc(points[i].x, points[i].y, 5, 0, Math.PI * 2);
+    ctx.fill();
   }
 }
 
-// Кластеризация точек
-function clusterPoints()
- {
-  let clusters = {}; // Создаем объект для хранения кластеров
+function clusterPoints() {
+  // Случайным образом выбираем начальные центры кластеров
+  for (let i = 0; i < k; i++) {
+    const randomIndex = Math.floor(Math.random() * points.length);
+    clusters.push({
+      center: points[randomIndex],
+      points: []
+    });
+  }
 
-  // Разбиение точек на кластеры
-  for (let i = 0; i < points.length; i++) 
-  {
-    let closestCluster = null;  // Указываем, что ближайший кластер еще не найден
-    let closestDistance = Infinity;   // Указываем большое расстояние до ближайшего кластера
+  // Кластеризация точек
+  let isChanged = true;
+  while (isChanged) {
+    // Очищаем точки у всех кластеров
+    for (let i = 0; i < clusters.length; i++) {
+      clusters[i].points = [];
+    }
 
-    for (let cluster in clusters) // Ищем ближайший кластер для текущей точки
-     {
-      let distance = Math.sqrt((points[i].x - clusters[cluster].center.x) ** 2 + (points[i].y - clusters[cluster].center.y) ** 2);
-      if (distance < closestDistance)
-       {
-        closestDistance = distance;
-        closestCluster = cluster;
+    isChanged = false;
+    // Кластеризуем точки
+    for (let i = 0; i < points.length; i++) {
+      let closestCluster = null;
+      let closestDistance = Infinity;
+      for (let j = 0; j < clusters.length; j++) {
+        const distance = Math.sqrt((points[i].x - clusters[j].center.x) ** 2 + (points[i].y - clusters[j].center.y) ** 2);
+        if (distance < closestDistance) {
+          closestDistance = distance;
+          closestCluster = j;
+        }
+      }
+      clusters[closestCluster].points.push(points[i]);
+    }
+
+    // Обновляем центры кластеров
+    for (let i = 0; i < clusters.length; i++) {
+      const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+      ctx.fillStyle = color;
+      for (let j = 0; j < clusters[i].points.length; j++) {
+        ctx.beginPath();
+        ctx.arc(clusters[i].points[j].x, clusters[i].points[j].y, 5, 0, Math.PI * 2);
+        ctx.fill();
+      }
+      clusters[i].center.x = clusters[i].points.reduce((sum, p) => sum + p.x, 0) / clusters[i].points.length;
+      clusters[i].center.y = clusters[i].points.reduce((sum, p) => sum + p.y, 0) / clusters[i].points.length;
+    }
+
+    // Отображение кластеров на холсте
+    for (let i = 0; i < clusters.length; i++) {
+      const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.arc(clusters[i].center0).toString(16).padStart(6, '0');
+      ctx.fillStyle = color;
+      for (let i = 0; i < clusters.length; i++) {
+        const center = clusters[i].center;
+        const points = clusters[i].points;
+        let totalX = 0;
+        let totalY = 0;
+        for (let j = 0; j < points.length; j++) {
+          totalX += points[j].x;
+          totalY += points[j].y;
+        }
+        const newCenter = {
+          x: totalX / points.length,
+          y: totalY / points.length
+        };
+        if (newCenter.x !== center.x || newCenter.y !== center.y) {
+          isChanged = true;
+        }
+        clusters[i].center = newCenter;
+        ctx.beginPath();
+        ctx.arc(newCenter.x, newCenter.y, 10, 0, Math.PI * 2);
+        ctx.fill();
       }
     }
-    if (closestDistance < clusterSize) // Если ближайшего кластера нет, создаем новый кластер для текущей точки
-     {
-      clusters[closestCluster].points.push(points[i]);
-      clusters[closestCluster].center.x = (clusters[closestCluster].center.x * (clusters[closestCluster].points.length - 1) + points[i].x) / clusters[closestCluster].points.length;
-      clusters[closestCluster].center.y = (clusters[closestCluster].center.y * (clusters[closestCluster].points.length - 1) + points[i].y) / clusters[closestCluster].points.length;
-    } 
-    else 
-    {
-      clusters[i] = 
-      {
-        points: [points[i]],
-        center: {
-          x: points[i].x,
-          y: points[i].y
-        }
-      };
+  }
+
+  // Отображение кластеров на холсте
+  for (let i = 0; i < clusters.length; i++) {
+    const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    ctx.fillStyle = color;
+    for (let j = 0; j < clusters[i].points.length; j++) {
+      ctx.beginPath();
+      ctx.arc(clusters[i].points[j].x, clusters[i].points[j].y, 5, 0, Math.PI * 2);
+      ctx.fill();
     }
   }
 
-  // Отрисовка кластеров на холсте
-  let colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff', '#c0c0c0', '#808080', '#800000', '#808000', '#008000', '#800080'];
-  let colorIndex = 0;
-  ctx.fillStyle = '#000';
-  ctx.font = '12px Arial';
-
-  for (let cluster in clusters) 
-  {
-    ctx.fillStyle = colors[colorIndex];
-    colorIndex++;
-    if (colorIndex >= colors.length) 
-    
-    {
-      colorIndex = 0;
-    }
-    ctx.fillRect(clusters[cluster].center.x - 10, clusters[cluster].center.y - 10, 20, 20);
-    ctx.fillText(`Кластер ${cluster}: ${clusters[cluster].points.length} точки`, clusters[cluster].center.x - 10, clusters[cluster].center.y + 25);
+  // Отображение центров кластеров на холсте
+  for (let i = 0; i < clusters.length; i++) {
+    const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(clusters[i].center.x, clusters[i].center.y, 10, 0, Math.PI * 2);
+    ctx.fill();
   }
 }
+function resetPoints() {
+  points = [];
+  clusters = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-// Обработчик нажатия на кнопку "Cluster"
-clusterButton.onclick = () => 
-{
-  clusterPoints();
-};
-
-// Обработчик клика на холсте для добавления точек
 canvas.addEventListener('click', addPoint);
-
-drawPoints();
-
-const resetButton = document.getElementById('resetButton'); // Получаем кнопку для сброса точек
-
-// Обработчик клика на кнопке "Reset"
-resetButton.onclick = () => 
-{
-  points = []; // Очищаем массив точек
-  drawPoints(); // Перерисовываем точки на холсте
-};
+clusterButton.addEventListener('click', clusterPoints);
+resetButton.addEventListener('click', resetPoints);
